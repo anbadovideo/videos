639a76616bbe6785b9d5fd47299f16d7fed5d09d
Chiwan Park <chiwanpark@icloud.com>
2015-03-07 20:55:40 +0900

    Add get_extractors API

--- videos/__init__.py
+++ videos/__init__.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, jsonify
+from youtube_dl import YoutubeDL, gen_extractors
 
 
 app = Flask(__name__)
+
+
+class VideosDL(YoutubeDL):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_default_info_extractors()
+
+
+@app.route('/extractors')
+def get_extractors():
+    extractors = [dict(name=ie.IE_NAME, working=ie.working()) for ie in gen_extractors()]
+    return jsonify(extractors=extractors)
